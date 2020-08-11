f28baaeeaf15f70bb5139b822655b56443b75e8f
Rohan Kumar <rohankumar1707@gmail.com>
2020-08-11 20:09:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -324,3 +324,34 @@ while(True):
 cap.release()
 cv2.destroyAllWindows()
 out.release()
+
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('http://192.168.18.4:8080/video')
+
+# This is how we define our codec
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+width = int(cap.get(3))
+height = int(cap.get(4))
+
+# Save our video at 20.0 FPS with the defined dimensions, Note my default camera has these dimensions, yours might be different
+out = cv2.VideoWriter(r'sampletestnew2.mp4',fourcc, 15.0, (width, height))
+
+while(True):
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Save this frame
+    out.write(frame)
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# Release the video writer Object
+out.release()
+cap.release()
+cv2.destroyAllWindows()
