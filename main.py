4943be834188d954d092304e6e1e14f0cdb1e1d9
Rohan Kumar <rohankumar1707@gmail.com>
2020-08-16 20:18:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,148 +4,9 @@ import cv2
 import time
 import datetime
 from collections import deque
-
-
-# Set Window normal so we can resize it
-cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-
-# Note the starting time
-start_time = time.time()
-
-# Initialize these variables for calculating FPS
-fps = 0 
-frame_counter = 0
-
-# Read the video steram from the camera
-cap = cv2.VideoCapture('http://192.168.18.4:8080/video')
-
-while(True):
-    
-    ret, frame = cap.read()
-    if not ret:
-        break 
-    
-    # Calculate the Average FPS
-    frame_counter += 1
-    fps = (frame_counter / (time.time() - start_time))
-    
-    # Display the FPS
-    cv2.putText(frame, 'FPS: {:.2f}'.format(fps), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255),1)
-    
-    # Show the Frame
-    cv2.imshow('frame',frame)
-    
-    # Exit if q is pressed.
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Release Capture and destroy windows
-cap.release()
-cv2.destroyAllWindows()
-
 from twilio.rest import Client
 
-# Read text from the credentials file and store in data variable
-with open('credentials.txt', 'r') as myfile:
-  data = myfile.read()
-
-# Convert data variable into dictionary
-info_dict = eval(data)
-
-# Your Account ROHAN from twilio.com/console
-account_rohan = info_dict['account_rohan']
-
-# Your Auth Token from twilio.com/console
-auth_token  = info_dict['auth_token']
-
-# Set client and send the message
-client = Client(account_rohan, auth_token)
-message = client.messages.create( to =info_dict['9922860648'], from_ = info_dict['Apllication'], body= "ALERT!!!  Intruder detected!!")
-
-# load a video
-cap = cv2.VideoCapture('sample_video.mp4')
-
-# Create the background subtractor object
-foog = cv2.createBackgroundSubtractorMOG2( detectShadows = True, varThreshold = 50, history = 2800)
-
-while(1):
-    
-    ret, frame = cap.read() 
-    if not ret:
-        break
-        
-    # Apply the background object on each frame
-    fgmask = foog.apply(frame)
-    
-    # Get rid of the shadows
-    ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
-    
-    # Show the background subtraction frame.
-    cv2.imshow('All three',fgmask)
-    k = cv2.waitKey(10)
-    if k == 27: 
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-# initlize video capture object
-cap = cv2.VideoCapture('sample_video.mp4')
-
-# you can set custom kernel size if you want
-kernel= None
-
-# initilize background subtractor object
-foog = cv2.createBackgroundSubtractorMOG2( detectShadows = True, varThreshold = 50, history = 2800)
-
-# Noise filter threshold
-thresh = 1100
-
-while(1):
-    ret, frame = cap.read()
-    if not ret:
-        break
-        
-    # Apply background subtraction
-    fgmask = foog.apply(frame)
-    
-    # Get rid of the shadows
-    ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
-    
-    # Apply some morphological operations to make sure you have a good mask
-  # fgmask = cv2.erode(fgmask,kernel,iterations = 1)
-    fgmask = cv2.dilate(fgmask,kernel,iterations = 4)
-    
-    # Detect contours in the frame
-    contours, hierarchy = cv2.findContours(fgmask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    
-    if contours:
-        
-        # Get the maximum contour
-        cnt = max(contours, key = cv2.contourArea)
-
-
-        # make sure the contour area is somewhat hihger than some threshold to make sure its a person and not some noise.
-        if cv2.contourArea(cnt) > thresh:
-
-            # Draw a bounding box around the person and label it as person detected
-            x,y,w,h = cv2.boundingRect(cnt)
-            cv2.rectangle(frame,(x ,y),(x+w,y+h),(0,0,255),2)
-            cv2.putText(frame,'Person Detected',(x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, cv2.LINE_AA)
-
- 
-    # Stack both frames and show the image
-    fgmask_3 = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-    stacked = np.hstack((fgmask_3,frame))
-    cv2.imshow('Combined',cv2.resize(stacked,None,fx=0.65,fy=0.65))
-
-    k = cv2.waitKey(40) & 0xff
-    if k == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
+#Function for sending message via Twilio
 def send_message(body, info_dict):
 
     # Your Account ROHAN from twilio.com/console
@@ -157,9 +18,10 @@ def send_message(body, info_dict):
 
     client = Client(account_rohan, auth_token)
 
-    message = client.messages.create( to = info_dict['9922860648'], from_ = info_dict['Apllication'], body= body)
+    message = client.messages.create( to = info_dict['9922860648'], from_ = info_dict['Application'], body= body)
     
-    def is_person_present(frame, thresh=1100):
+#Function for detecting if a person is present or not
+def is_person_present(frame, thresh=1100):
     
     global foog
     
@@ -192,15 +54,14 @@ def send_message(body, info_dict):
     # Otherwise report there was no one present
     else:
         return False, frame
-
     
-    #time.sleep(15)
+#time.sleep(15)
 
 # Set Window normal so we can resize it
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 
 # This is a test video
-cap = cv2.VideoCapture('http://192.168.18.4:8080/video')
+cap = cv2.VideoCapture('http://192.168.18.4:6067/video')
 
 # Get width and height of the frame
 width = int(cap.get(3))
@@ -242,8 +103,7 @@ while(True):
     if not ret:
         break 
             
-    # This function will return a boolean variable telling if someone was present or not, it will also draw boxes if it 
-    # finds someone
+    # This function will return a boolean variable telling if someone was present or not, it will also draw boxes if it finds someone
     detected, annotated_image = is_person_present(frame)  
     
     # Register the current detection status on our deque object
@@ -259,8 +119,7 @@ while(True):
     # If status is True but the person is not in the current frame
     if status and not detected:
         
-        # Restart the patience timer only if the person has not been detected for a few frames so we are sure it was'nt a 
-        # False positive
+        # Restart the patience timer only if the person has not been detected for a few frames so we are sure it wasn't a False positive
         if sum(de) > (detection_thresh/2): 
             
             if initial_time is None:
@@ -285,16 +144,6 @@ while(True):
     
     # Get the current time in the required format
     current_time = datetime.datetime.now().strftime("%A, %I:%M:%S %p %d %B %Y")
-
-    # Display the FPS
-    cv2.putText(annotated_image, 'FPS: {:.2f}'.format(fps), (510, 450), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 40, 155),2)
-    
-    # Display Time
-    cv2.putText(annotated_image, current_time, (310, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255),1)    
-    
-    # Display the Room Status
-    cv2.putText(annotated_image, 'Room Occupied: {}'.format(str(status)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, 
-                (200, 10, 150),2)
 
     # Show the patience Value
     if initial_time is None:
@@ -323,35 +172,4 @@ while(True):
 # Release Capture and destroy windows
 cap.release()
 cv2.destroyAllWindows()
-out.release()
-
-
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture('http://192.168.18.4:8080/video')
-
-# This is how we define our codec
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-width = int(cap.get(3))
-height = int(cap.get(4))
-
-# Save our video at 20.0 FPS with the defined dimensions, Note my default camera has these dimensions, yours might be different
-out = cv2.VideoWriter(r'sampletestnew2.mp4',fourcc, 15.0, (width, height))
-
-while(True):
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    # Save this frame
-    out.write(frame)
-
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-# Release the video writer Object
-out.release()
-cap.release()
-cv2.destroyAllWindows()
+out.release()    
